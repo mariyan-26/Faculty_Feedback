08e54b2bd62c219b47bf8d6fa014f6e1c426f77b
RA <rubenanju@Ruben.local>
2026-03-22 09:40:30 +0530

    changes

--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kjsdc-10/Downloads/faculty_dashboard/backend/.venv"
+set -gx VIRTUAL_ENV "/Users/rubenanju/Faculty_Feedback/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
